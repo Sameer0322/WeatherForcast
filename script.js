17d9521badbdc6f659b4219ee890afa3d08b96b1
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2023-08-26 20:03:59 +0530

    update

--- script.js
+++ script.js
@@ -9,24 +9,19 @@ const options = {
 };
 
 const getWeather = (city)=>{
+
+    
     cityName.innerHTML = city
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, options)
     .then(response => response.json())
     .then(response => {
         console.log(response)
         cloud_pct.innerHTML  = response.cloud_pct
-        feels_like.innerHTML  = response.feels_like
         humidity.innerHTML  = response.humidity
-        humidity2.innerHTML  = response.humidity
         max_temp.innerHTML  = response.max_temp
         min_temp.innerHTML  = response.min_temp
-        sunrise.innerHTML  = response.sunrise
-        sunset.innerHTML  = response.sunset
         temp.innerHTML  = response.temp
-        temp2.innerHTML  = response.temp
-        // wind_degrees.innerHTML  = response.wind_degrees
         wind_speed.innerHTML  = response.wind_speed
-        wind_speed2.innerHTML  = response.wind_speed
     })
     .catch(err => console.error(err));
 
